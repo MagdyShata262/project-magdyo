58fad5c5e7874c48b06b0000954971e22a6bf58e
Magdy Shata <magdyshata262@gmail.com>
2025-10-04 15:43:15 +0300

    add-4

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcome } from './nx-welcome';
+import{Button, Footer, Header, Input, ProductCard} from '@project-magdyo/ui';
+import{Card} from '@project-magdyo/ui';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [NxWelcome, RouterModule,Button,Card,ProductCard,Header,Input,Footer ],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
